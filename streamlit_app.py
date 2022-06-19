9ea0e4ec516813827fe9e8638e4bf5bf9d62a53c
Sebastien MASSIOT <edenwebagency@gmail.com>
2022-06-19 12:38:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,8 +72,9 @@ Triste, et le jour pour moi sera comme la nuit."""
 
             query = requests.post("http://localhost:5000/generate", params=payload)
             response = query.json()            
-            rep = response["""prompt"""] + response["""text"""]             
-            inp = text_area.text_area("ou ecrivez votre propre suggestion ici!",rep,  max_chars=10000, height=600)
+            rep = response["""prompt"""] + response["""text"""]  
+            inp = rep
+            text_area.text_area("ou ecrivez votre propre suggestion ici!",rep,  max_chars=10000, height=600)
             
            
 
