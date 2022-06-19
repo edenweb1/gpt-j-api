82df6e1a2b800247e42ebac62bf20fb9a9eea249
Sebastien MASSIOT <edenwebagency@gmail.com>
2022-06-19 16:00:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,7 +73,7 @@ with st.form(key="inputs"):
             response = query.json()            
             rep = response["""prompt"""] + response["""text"""]             
             inp = text_area.text_area("ou ecrivez votre propre suggestion ici!",rep,  max_chars=10000, height=600)
-	    st.session_state.example = rep
+	    st.session_state.example = 'rep'
             
            
             
