3c15e8d41994fcc7e9969ee80e9bb05e6d071bec
Sebastien MASSIOT <edenwebagency@gmail.com>
2022-06-19 16:43:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,8 @@ example = st.selectbox("Choisissez votre suggestion", ex_names)
 text_area = st.empty()
 
 if 'example' not in st.session_state:
- prompt = example              
- inp = text_area.text_area("ou ecrivez votre propre suggestion ici!", prompt , max_chars=20000, height=600)   
+       
+ inp = text_area.text_area("ou ecrivez votre propre suggestion ici!", example , max_chars=20000, height=600)   
 else:
  #st.write(st.session_state.example)	
  inp = text_area.text_area("ou ecrivez votre propre suggestion ici!", st.session_state.example , max_chars=20000, height=600) 
