95bcab289017a24532f3deafeb85df056e0a0ea3
Sebastien MASSIOT <edenwebagency@gmail.com>
2022-06-19 13:56:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ Triste, et le jour pour moi sera comme la nuit."""    ]
     example = st.selectbox("Choisissez votre suggestion", ex_names)
     text_area = st.empty()
     inp = text_area.text_area(
-        "ou ecrivez votre propre suggestion ici!", example, max_chars=20000, height=600
+        "ou ecrivez votre propre suggestion ici!", "", max_chars=20000, height=600
     )   
 
     with st.beta_expander("Options de generations"):
